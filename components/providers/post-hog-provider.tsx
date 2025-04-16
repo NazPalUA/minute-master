1dba193e7d2c51ad45b57ffa12ea59b9eb3062d8
Nazar Palamarchuk <ua.nazar.palamarchuk.ua@gmail.com>
2025-04-16 16:38:57 +0300

    fix: update PostHogProvider configuration to use specific ingest and UI hosts for improved tracking

--- components/providers/post-hog-provider.tsx
+++ components/providers/post-hog-provider.tsx
@@ -11,7 +11,8 @@ import { PostHogProvider as PHProvider } from 'posthog-js/react'
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
-      api_host: env.NEXT_PUBLIC_POSTHOG_HOST,
+      api_host: '/ingest',
+      ui_host: 'https://eu.posthog.com',
       person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
       capture_pageview: false // Disable automatic pageview capture, as we capture manually
     })
