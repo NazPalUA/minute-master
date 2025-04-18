0c6dfb9ed2c4e05c2d0df69a6a14d17a2e8b4b54
Nazar Palamarchuk <ua.nazar.palamarchuk.ua@gmail.com>
2025-04-18 21:50:16 +0300

    refactor: reorganize time utility functions and introduce new date formatting capabilities for enhanced time management

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -1,15 +1,4 @@
 export { cn } from './cn'
-export { createDateFormatter, dateLocaleMap } from './date-formater'
-export {
-  formatTimeToHoursAndMinutes,
-  padAndJoinTimeValues
-} from './format-time'
-export { formatDuration } from './format-time-duration'
 export { getChartColorsArr } from './get-chart-colors-arr'
-export { isoWeekToDate } from './iso-week-to-date'
-export {
-  millisecondsToTimeParts,
-  type TimeParts
-} from './milliseconds-to-time-parts'
-export * from './timeConvertors'
+export * from './time'
 export { asyncTryCatch, tryCatch } from './try-catch'
---
+++ lib/utils/time/index.ts
@@ -0,0 +1,12 @@
+export { createDateFormatter, dateLocaleMap } from './date-formater'
+export { formatDuration } from './format-duration'
+export {
+  formatTimeToHoursAndMinutes,
+  padAndJoinTimeValues
+} from './format-time'
+export { isoWeekToDate } from './iso-week-to-date'
+export {
+  millisecondsToTimeParts,
+  type TimeParts
+} from './milliseconds-to-time-parts'
+export * from './timeConvertors'
